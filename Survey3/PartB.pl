467a5589b18d2356f4b4b9c8671292abe042f67d
carterc64 <89395690+carterc64@users.noreply.github.com>
2022-12-17 20:30:48 -0500

    Add files via upload

---
+++ Survey3/PartB.pl
@@ -0,0 +1,16 @@
+Carter Copen
+This assignment follows academic integrity.
+
+
+pivot(Before, After) :-
+    length(Before, N),
+    Half is N - N // 2,
+    length(H1, Half),
+    append(H1, H2, Before),
+    append(H2, H1, After).
+
+
+
+
+
+    
